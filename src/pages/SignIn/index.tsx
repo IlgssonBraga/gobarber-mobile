bbdca4c5cd9ef268d2c81a98fe5f9910fc264d06
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-05-11 18:40:52 -0300

    feat: button and input components

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
 
-import { Container } from './styles';
+import logo from '../../assets/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
     return (
         <Container>
-            <Text>SignIn</Text>
+            <Image source={logo} />
+            <Title>Faça seu logon</Title>
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+            <Button onPress={() => {}}>Entrar</Button>
         </Container>
     );
 };
