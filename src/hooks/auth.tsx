a1485b94b43b28bdd7c95471ff1692a5697426d3
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-06-10 17:02:37 -0300

    feat: dashboard

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -8,6 +8,13 @@ import React, {
 import AsyncStorage from '@react-native-community/async-storage';
 import api from '../services/api';
 
+interface User {
+    id: string;
+    name: string;
+    email: string;
+    avatar_url: string;
+}
+
 interface SignInCredentials {
     email: string;
     password: string;
@@ -15,11 +22,11 @@ interface SignInCredentials {
 
 interface AuthState {
     token: string;
-    user: object;
+    user: User;
 }
 
 interface AuthContextData {
-    user: object;
+    user: User;
     loading: boolean;
     signin(credentials: SignInCredentials): Promise<void>;
     signout(): void;
@@ -38,6 +45,7 @@ export const AuthProvider: React.FC = ({ children }) => {
             ]);
 
             if (token[1] && user[1]) {
+                api.defaults.headers.authorization = `Bearer ${token[1]}`;
                 setData({ token: token[1], user: JSON.parse(user[1]) });
             }
 
@@ -57,6 +65,8 @@ export const AuthProvider: React.FC = ({ children }) => {
             ['@GoBarber:token', token],
             ['@GoBarber:user', JSON.stringify(user)],
         ]);
+
+        api.defaults.headers.authorization = `Bearer ${token}`;
 
         setData({ token, user });
     }, []);
